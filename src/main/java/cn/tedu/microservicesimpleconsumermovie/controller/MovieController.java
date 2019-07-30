c846e9f51b226edd57c85d05156f536c126ffa31
zhanghao <vip420279007@163.com>
2019-07-30 16:18:15 +0800

    服务注册到需认证的eureka集群中

--- src/main/java/cn/tedu/microservicesimpleconsumermovie/controller/MovieController.java
+++ src/main/java/cn/tedu/microservicesimpleconsumermovie/controller/MovieController.java
@@ -2,10 +2,14 @@ package cn.tedu.microservicesimpleconsumermovie.controller;
 
 import cn.tedu.microservicesimpleconsumermovie.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * Description :
@@ -17,9 +21,20 @@ import org.springframework.web.client.RestTemplate;
 public class MovieController {
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id){
         return this.restTemplate.getForObject("http://localhost:8090/"+id, User.class);
+    }
+
+    /**
+     * 查询microservice-provider-user服务的信息并返回
+     * @return microservice-provider-user服务的信息
+     */
+    @GetMapping("/user-instance")
+    public List<ServiceInstance> showInfo(){
+        return this.discoveryClient.getInstances("microservice-provider-user");
     }
 }
